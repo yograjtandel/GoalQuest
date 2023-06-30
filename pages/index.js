dca1a115b55aaee7596f03e303874b7889484608
yograjtandel <yograjtandel9@gmail.com>
2023-06-30 07:47:42 +0530

    signup test

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,28 @@
 import { useSession, signIn, signOut } from 'next-auth/react';
 
 export default function Home() {
+
+async function signup() {
+    debugger
+  const auth0Response = await fetch(
+    `https://dev-c0a3javl4d5ua32y.us.auth0.com/dbconnections/signup`,
+    {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        client_id: "4z8QNi1ATEjLsRiK5XJORcO2xif7ffmn",
+        connection: 'Username-Password-Authentication',
+        email: "new@new.com",
+        password: "New@1234",
+      }),
+    }
+  )
+  debugger
+  const body = await auth0Response.json()
+  
+}
   const { data: session } = useSession();
   if (session) {
     return (
@@ -10,10 +32,26 @@ export default function Home() {
       </>
     );
   }
+
+
   return (
     <>
       Not signed in <br />
       <button onClick={() => signIn()}>Sign in</button>
+      <button
+        onClick={() =>
+          signIn(
+            'auth0',
+            { callbackUrl: 'http://localhost:3000' },
+            { screen_hint: 'signup' }
+          )
+        }
+      >
+        Sign up
+      </button>
+
+      <button onClick={() => signup()}>test sign up</button>
+
     </>
   );
 }
--- pages/setting/index.js
+++ pages/setting/index.js
@@ -116,5 +116,5 @@ export const getServerSideProps = async () => {
     url: '/v1/rights/',
   });
 
-  return { props: {} };
+  return { props: {rights: res.data} };
 };
